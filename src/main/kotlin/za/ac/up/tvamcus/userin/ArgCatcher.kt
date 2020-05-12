04094e007578404dacbf7bc73191e7200513682b
Siocnarff <josuabotha@protonmail.com>
2020-05-12 08:53:17 +0200

    ArgCatcher sufficient options created

--- src/main/kotlin/za/ac/up/tvamcus/userin/ArgCatcher.kt
+++ src/main/kotlin/za/ac/up/tvamcus/userin/ArgCatcher.kt
@@ -7,16 +7,7 @@ object ArgCatcher {
     @JvmStatic
     fun main(args: Array<String>) {
         val parser: CommandLineParser = DefaultParser()
-        val options: Options = Options()
-        val ef: Option = Option(
-            "ef", "evaluateFile", true, "Runs an evaluation of your file - reachability of given location by any process is tested by default - args are filename and error location"
-        )
-        ef.args = 2
-        options.addOption(ef)
-        options.addOption("l", "liveness", false, "Set evaluation of file to liveness - if this flag is not set it is assumed you are testing for reachability")
-        options.addOption("p", "processes", true, "Accepts a comma-separated list of processes ids that should take part in evaluation - if not used all processes take part")
-        options.addOption("f", "fair", false, "Enforce fairness")
-        options.addOption("h", "help", false, "Prints this help message")
+        val options = setArgOptions()
         val cmd: CommandLine = parser.parse(options, args)
         if(cmd.hasOption("ef")) {
             println("evaluate file")
@@ -26,5 +17,55 @@ object ArgCatcher {
             val formatter = HelpFormatter()
             formatter.printHelp("Tvamcus", options);
         }
+    }
+
+    private fun setArgOptions(): Options {
+        val options = Options()
+        val efr = Option(
+            "er",
+            "evaluateReachability",
+            true,
+            "Evaluate reachability by any process in CFG model - args are JSON filename, error location and upper bound."
+        )
+        efr.args = 3
+        options.addOption(efr)
+        val efl = Option(
+            "el",
+            "evaluateLiveness",
+            true,
+            "Evaluate liveness of all processes in CFG model - args are JSON filename, error location and upper bound."
+        )
+        efl.args = 3
+        options.addOption(efl)
+        options.addOption(
+            "f",
+            "fair",
+            false,
+            "Enforce fairness."
+        )
+        options.addOption(
+            "h",
+            "help",
+            false,
+            "Print this help message."
+        )
+        options.addOption(
+            "led",
+            "locationEncodingDisjunct",
+            false,
+            "Use disjunction in error location encoding, if flag not specified error location encoding uses conjunction."
+        )
+        options.addOption(
+            "mm",
+            "multiModel",
+            false,
+            "Set evaluation to use the multi-model approach. NoP file must have same name as AllP file, only with _0P appended.")
+        options.addOption(
+            "p",
+            "processes",
+            true,
+            "Accepts a comma-separated list of processes (by id) that should take part in evaluation, if not specified all processes take part."
+        )
+        return options;
     }
 }
